4d458fc369fcdc2ddda4565a84c1c417c488c342
sedricogenrwoth <sedricogenrwot1@gmail.com>
2026-04-12 21:42:02 +0300

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -16,3 +16,27 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Groups from "./pages/Groups";
+import CreateGroup from "./pages/CreateGroup";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/create-group" element={<CreateGroup />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
