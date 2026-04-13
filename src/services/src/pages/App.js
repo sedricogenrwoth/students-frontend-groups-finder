fbc550af858db4151002934e2975c29927f8b7dc
epajaanthony61-star <epajaanthony61@gmail.com>
2026-04-13 21:04:50 +0300

    Update App.js

--- src/services/src/pages/App.js
+++ src/services/src/pages/App.js
@@ -1,2 +1,5 @@
 import Groups from "./pages/Groups";
 <Route path="/groups" element={<Groups />} />
+
+ import CreateGroup from "./pages/CreateGroup";
+ <Route path="/create-group" element={<CreateGroup />} />
