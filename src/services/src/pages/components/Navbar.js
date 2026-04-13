85693792710916ae457d0cdef43312060e22e313
ayella isaac <oumaisaac607@gmail.com>
2026-04-13 21:08:54 +0300

    updating navbar

--- src/services/src/pages/components/Navbar.js
+++ src/services/src/pages/components/Navbar.js
@@ -9,3 +9,14 @@ function Navbar() {
 }
 
 export default Navbar;
+function Navbar() {
+    return (
+        <div>
+            <a href="/dashboard">Dashboard</a> |
+            <a href="/groups">Groups</a> |
+            <a href="/create-group">Create Group</a>
+        </div>
+    );
+}
+
+export default Navbar;
