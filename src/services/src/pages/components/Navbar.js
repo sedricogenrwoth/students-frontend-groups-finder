c69778193b80c7c8a6c2e6221627f41dfebf9c7b
Stephen10piramoe <stephenpiramoe@gmail.com>
2026-04-13 20:24:50 +0300

    Navbar

---
+++ src/services/src/pages/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <div>
+            <a href="/dashboard">Dashboard</a> |
+            <a href="/groups">Groups</a> |
+            <a href="/create-group">Create Group</a>
+        </div>
+    );
+}
+
+export default Navbar;
