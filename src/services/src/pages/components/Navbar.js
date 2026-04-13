cf2546430c97e29677868caa1f0ae5c340ee28db
sedricogenrwoth <sedricogenrwot1@gmail.com>
2026-04-13 21:24:27 +0300

    navbar

--- src/services/src/pages/components/Navbar.js
+++ src/services/src/pages/components/Navbar.js
@@ -20,3 +20,16 @@ function Navbar() {
 }
 
 export default Navbar;
+function Navbar() {
+    return (
+        <div>
+            <a href="/dashboard">Dashboard</a> | 
+            <a href="/groups">Groups</a> | 
+            <a href="/create-group">Create Group</a> | 
+            <a href="/sessions">Sessions</a> | 
+            <a href="/posts">Chat</a>
+        </div>
+    );
+}
+
+export default Navbar;
